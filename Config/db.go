03a1c330e990ed25c7e262ea43c70b01ffdaa6d4
Razik Shariff <razik.shariff@zopping.com>
2025-08-21 15:53:06 +0530

    gofr sofr

---
+++ Config/db.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var Api=os.Getenv("API_KEY")
+func ConnectDB() {
+	// Load from env vars
+	username := "sql12794681"
+	rawPassword := "eYCvw4Gty2"
+	password := url.QueryEscape(rawPassword)
+	host := "sql12.freesqldatabase.com"
+	port := "3306"
+	dbname := "sql12794681"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username, password, host, port, dbname)
+
+	// Open DB connection
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("❌ failed to connect to database: %v", err)
+	}
+
+	DB = db
+	log.Println("✅ Database connected successfully")
+}
